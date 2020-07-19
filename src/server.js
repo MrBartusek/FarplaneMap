5174fe07d5aab3f46ec3465d80f83a7d86552aa3
MrBartusek <bartusekcraft@gmail.com>
2020-07-19 21:44:55 +0200

    Add players data fetching to backend

--- src/server.js
+++ src/server.js
@@ -32,14 +32,27 @@ app.get('/get-data', async (req, res) =>
 
 		let result = { requestedAt: String(+new Date)};
 	
+		const leaderboard = await doc.sheetsByIndex[0].getRows({offset: 2});
+		const playerData = await doc.sheetsByIndex[1].getRows();
 		const tasks = await doc.sheetsByIndex[2].getRows();
 		result.tasks = praseTasks(tasks);
+		result.players = prasePlayers(leaderboard, playerData, result.tasks);
 
 		fs.writeFileSync(cacheFileDir, JSON.stringify(result));
 
 		res.status(200).json({ cache: false, ...result});
 	}
 });
+
+function cacheAvailable()
+{
+	if(!fs.existsSync(cacheFileDir))
+	{
+		return false;
+	}
+	const data = JSON.parse(fs.readFileSync(cacheFileDir ).toString());
+	return +new Date - parseInt(data.requestedAt) < 3600000; // One hour in ms
+}
 
 function praseTasks(rows)
 {
@@ -64,12 +77,71 @@ function praseTasks(rows)
 	return tasks;
 }
 
-function cacheAvailable()
+function prasePlayers(leaderboard, playersData, parsedTasks)
 {
-	if(!fs.existsSync(cacheFileDir))
+	result = praseLeaderboard(leaderboard);
+
+	const taskNames =  playersData[0]._rawData;
+	const validTasksCount = playersData[0]._sheet.headerValues.findIndex((x) => x.includes('Old-'));
+
+	for (let i = 3; i < playersData.length; i++) 
 	{
-		return false;
+		const playerData = playersData[i];
+		const leaderboardPlayerId = leaderboard.findIndex((p) => p.name === playerData.name);
+		if(leaderboardPlayerId === undefined)
+		{
+			console.error(`Found player ${leaderboardPlayerId.name} in "Leaderboard" but not in "Player Data"`);
+			continue;
+		}
+
+		result[leaderboardPlayerId].completedTasks = [];
+		
+		for (let i = 2; i < validTasksCount; i++) 
+		{
+			const taskExperience = playerData._rawData[i];
+			if(taskExperience && taskExperience.length > 1)
+			{
+				const taskId = parsedTasks.findIndex((t) => t.name == taskNames[i]);
+				if(taskId != undefined)
+				{
+					result[leaderboardPlayerId].completedTasks.push(taskId);
+				}
+				else
+				{
+					console.error(`Found task ${taskNames[i]} in "Player Data" but not in "Tasks"`);
+				}
+			}
+		}
+		playerData.discord = prasePlayerDiscord();
 	}
-	const data = JSON.parse(fs.readFileSync(cacheFileDir ).toString());
-	return +new Date - parseInt(data.requestedAt) < 3600000; // One hour in ms
+
+	return result;
+}
+
+function praseLeaderboard(leaderboard)
+{
+	let players = [];
+	for(const player of leaderboard)
+	{
+		players.push({ 
+			name: player.name,
+			experience: player.experience
+		});
+	}
+	return players;
+}
+
+function prasePlayerDiscord(id)
+{
+	if(id != '')
+	{
+		return {
+			id: id,
+			//TODO: Add username and avatar there
+		};
+	}
+	else
+	{
+		return null;
+	}
 }
