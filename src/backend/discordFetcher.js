2a0376a233c5daacf238a1bf24c0960352a5ff3d
MrBartusek <bartusekcraft@gmail.com>
2020-08-12 17:22:06 +0200

    Working discord fetching

--- src/backend/discordFetcher.js
+++ src/backend/discordFetcher.js
@@ -1,11 +1,20 @@
+const fetch = require('node-fetch');
 
 class DiscordFetcher
 {
 	async getUser(id)
 	{
-		let result = { requestedAt: String(+new Date)};
-		result.id = id;
-		return result;
+		let result = { requestedAt: String(+new Date) };
+		return fetch('https://discord.com/api/v6/users/' + id, { headers: { Authorization: `Bot ${process.env.FARPLANE_DISCORD_KEY}`}})
+			.then(response => response.json())
+			.then((data) =>
+			{
+				console.log(data);
+				result.id = data.id;
+				result.username = data.username + '#' + data.discriminator;
+				result.avatar = `https://cdn.discordapp.com/avatars/${data.id}/a_${data.avatar}.png?size=256`;
+				return result;
+			});
 	}
 }
 
